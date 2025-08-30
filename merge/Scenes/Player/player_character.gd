f9833098beccc1b7e107f079aa29388aec5cab34
Dev Karan <devkaran7501@gmail.com>
2025-08-30 09:52:50 +0530

    Body swap extended to all parts

--- merge/Scenes/Player/player_character.gd
+++ merge/Scenes/Player/player_character.gd
@@ -1,21 +1,69 @@
+@tool
 extends Node2D
 
 var move_speed_pixels : int = 10
-var current_arm : Node2D = null
-@onready var arm_slot = $left_arm_slot
+var current_left_arm : Node2D = null
+var current_right_arm : Node2D = null
+var current_left_leg : Node2D = null
+var current_torso : Node2D = null
+var current_right_leg : Node2D = null
+
+
+@onready var left_arm_slot = $LeftArm
+@onready var head_slot = $Head
+@onready var right_arm_slot = $RightArm
+@onready var left_leg_slot = $LeftLeg
+@onready var torso_slot = $Torso
+@onready var right_leg_slot = $RightLeg
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	swap_arm("res://Scenes/Body Part Scenes/Arms/default_arm.tscn")
-
-func swap_arm(arm_path : String):
-	if current_arm:
-		current_arm.queue_free()
+	swap_left_arm("res://Scenes/Player/Body Part Scenes/RigArms/RigLeftArm.tscn")
+	swap_right_arm("res://Scenes/Player/Body Part Scenes/RigArms/RigRightArm.tscn")
+	swap_left_leg("res://Scenes/Player/Body Part Scenes/RigLegs/RigLeftLeg.tscn")
+	swap_torso("res://Scenes/Player/Body Part Scenes/RigTorso/RigTorso.tscn")
+	swap_right_leg("res://Scenes/Player/Body Part Scenes/RigLegs/RigRightLeg.tscn")
+	var head_scene = load("res://Scenes/Player/Body Part Scenes/RigHead/RigHead.tscn")
+	head_slot.add_child(head_scene.instantiate())
+	
+func swap_right_arm(arm_path: String):
+	if current_right_arm:
+		current_right_arm.queue_free()
 		
 	var arm_scene = load(arm_path)
-	current_arm = arm_scene.instantiate()
-	arm_slot.add_child(current_arm)
+	current_right_arm = arm_scene.instantiate()
+	right_arm_slot.add_child(current_right_arm)
 
+func swap_left_arm(arm_path : String):
+	if current_left_arm:
+		current_left_arm.queue_free()
+		
+	var arm_scene = load(arm_path)
+	current_left_arm = arm_scene.instantiate()
+	left_arm_slot.add_child(current_left_arm)
 
+func swap_left_leg(leg_path: String):
+	if current_left_leg:
+		current_left_leg.queue_free()
+	
+	var leg_scene = load(leg_path)
+	current_left_leg = leg_scene.instantiate()
+	left_leg_slot.add_child(current_left_leg)
+	
+func swap_torso(torso_path: String):
+	if current_torso:
+		current_torso.queue_free()
+	
+	var torso_scene = load(torso_path)
+	current_torso = torso_scene.instantiate()
+	torso_slot.add_child(current_torso)
+
+func swap_right_leg(leg_path: String):
+	if current_right_leg:
+		current_right_leg.queue_free()
+	
+	var leg_scene = load(leg_path)
+	current_right_leg = leg_scene.instantiate()
+	right_leg_slot.add_child(current_right_leg)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_right"):
@@ -31,7 +79,7 @@ func _process(delta: float) -> void:
 		position.y -= move_speed_pixels
 		
 	if Input.is_action_just_pressed("change_arm"):
-		swap_arm("res://Scenes/Body Part Scenes/Arms/test_arm.tscn")
+		swap_left_arm("res://Scenes/Player/Body Part Scenes/Arms/test_arm.tscn")
 	
 	if Input.is_action_just_pressed("attack"):
-		current_arm.play_attack()
+		current_left_arm.play_attack()
