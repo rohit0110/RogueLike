2976966b23e1f45effda54c758a6c2217e9bab6f
Dev Karan <devkaran7501@gmail.com>
2025-09-04 17:28:31 +0530

    feat: Added melee and ranged attack for test_arm and toechondria_arm

--- merge/Scenes/Player/Body Part Scenes/Arms/test_arm.gd
+++ merge/Scenes/Player/Body Part Scenes/Arms/test_arm.gd
@@ -1,6 +1,26 @@
 extends Node2D
 
-@onready var anim = $AnimationPlayer
+@export var animation_tree : AnimationTree
+var playback: AnimationNodeStateMachinePlayback
+var attack_state : bool = false
 
-func play_attack():
-	anim.play("attack")
+func _ready() -> void:
+	playback = animation_tree["parameters/playback"]
+	
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("attack"):
+		attack_state = true
+	select_animation()
+	update_animation_parameters()
+	
+func select_animation():
+	if attack_state:
+		playback.travel("attack")
+		await get_tree().create_timer(0.5).timeout
+		attack_state = false
+	else:
+		playback.travel("idle")
+
+func update_animation_parameters():
+	pass
